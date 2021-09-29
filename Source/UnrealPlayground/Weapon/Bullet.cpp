51ac677da519202b9290cf28ff288eba932f8d2a
David Nichol <5cents87@gmail.com>
2021-09-28 19:50:24 -0500

    Vault State
    Adds Vault State

--- Source/UnrealPlayground/Weapon/Bullet.cpp
+++ Source/UnrealPlayground/Weapon/Bullet.cpp
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
+=======
+>>>>>>> main
 #include "Bullet.h"
 #include "Components/StaticMeshComponent.h"
 #include <Runtime/Engine/Classes/Kismet/KismetMathLibrary.h>
 
+<<<<<<< HEAD
 // Sets default values
 ABullet::ABullet()
 {
@@ -27,15 +31,111 @@ ABullet::ABullet()
 	FRotator Rotation = FRotator(0.f, -90.f, 0.f);
 
 	Mesh->AddWorldRotation(Rotation);
+=======
+ABullet::ABullet()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	SetRootComponent(Mesh);
+	Mesh->SetCollisionProfileName("NoCollision");
+	SetActorEnableCollision(false);
+}
+
+void ABullet::InitializeOwner(const float Base, const float Body, const float Limb, const float Head, const float Range, const float FireSpeed)
+{
+	BaseDamage = Base;
+	BodyMultiplier = Body;
+	LimbMultiplier = Limb;
+	HeadMultiplier = Head;
+	MaxRange = Range;
+	Speed = FireSpeed;
+}
+
+void ABullet::BeginPlay()
+{
+	Super::BeginPlay();
+
+	PrimaryActorTick.SetTickFunctionEnable(false);
+	bIsActive = false;
+>>>>>>> main
 }
 
 void ABullet::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+<<<<<<< HEAD
 	if (RaycastHit.IsValidBlockingHit())
 	{
 		SetActorLocation(UKismetMathLibrary::VInterpTo_Constant(GetActorLocation(), RaycastHit.ImpactPoint, DeltaTime, 1400.f));
 	}
 }
 
+=======
+	const float MaxRangeSq = MaxRange * MaxRange;
+	const FVector Delta = TraceDirection * Speed * DeltaTime;
+	const FVector TraceEnd = CurrentTraceLocation + Delta;
+
+	SetActorLocation(GetActorLocation() + Delta);
+
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByChannel(Hit, CurrentTraceLocation, TraceEnd, ECC_Pawn, QueryParams);
+	
+	if (Hit.bBlockingHit)
+	{
+		if (Hit.Actor != nullptr)
+		{
+			float DamageFactor = BodyMultiplier;
+
+			if (Hit.BoneName == "Head")
+			{
+				DamageFactor = HeadMultiplier;
+			}
+
+			else if (Hit.BoneName == "Limb")
+			{
+				DamageFactor = LimbMultiplier;
+			}
+
+			const float Damage = DamageFactor * BaseDamage;
+			//We don't use this but its required by Unreal's damage events
+			//not sure if its safe to pass a nullptr
+			const FDamageEvent DamageEvent;
+
+			Hit.Actor->TakeDamage(Damage, DamageEvent, nullptr, this);
+
+			//TODO implement hit splat particle effects
+		}
+
+		SetIsActive(false);
+		return;
+	}
+
+	//Bullet traveled max distance
+	else if (FVector::DistSquared(TraceEnd, TraceStart) >= MaxRangeSq)
+	{
+		SetIsActive(false);
+		return;
+	}
+
+	CurrentTraceLocation = TraceEnd;
+}
+
+void ABullet::SetTrajectory(const FVector TraceOrigin, const FVector TraceVector, const FVector BulletVector)
+{
+	TraceStart = TraceOrigin;
+	CurrentTraceLocation = TraceOrigin;
+	TraceDirection = TraceVector;
+	BulletDirection = BulletVector;
+
+	SetIsActive(true);
+}
+
+void ABullet::SetIsActive(const bool Value)
+{
+	PrimaryActorTick.SetTickFunctionEnable(Value);
+	bIsActive = Value;
+	Mesh->SetVisibility(Value);
+}
+>>>>>>> main
