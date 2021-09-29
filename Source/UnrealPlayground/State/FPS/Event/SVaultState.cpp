9f8cc72d477b77efa1a70ddad27b8929cfaeb620
David Nichol <5cents87@gmail.com>
2021-09-28 19:50:24 -0500

    Vault State
    Adds Vault State

---
+++ Source/UnrealPlayground/State/FPS/Event/SVaultState.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SVaultState.h"
+
+void USVaultState::OnEnter()
+{
+	Super::OnEnter();
+
+	// Vault animation here
+
+	UE_LOG(LogTemp, Warning, TEXT("Good work. Go to bed now."));
+
+	//Shooter->GetArms()->GetAnimInstance()->Montage_Play();
+}
+
+void USVaultState::OnExit()
+{
+	Super::OnExit();
+}
+
+void USVaultState::Tick(const float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void USVaultState::Initialize(UStateMachine* StateMachine, UObject* ContextObject)
+{
+	Super::Initialize(StateMachine, ContextObject);
+}
