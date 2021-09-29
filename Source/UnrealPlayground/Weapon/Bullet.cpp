4519473bbf1947a5e73dda40931dca6ea7b1b915
David Nichol <5cents87@gmail.com>
2021-09-28 21:59:30 -0500

    Fixed vault object prompt

--- Source/UnrealPlayground/Weapon/Bullet.cpp
+++ Source/UnrealPlayground/Weapon/Bullet.cpp
@@ -1,37 +1,7 @@
-<<<<<<< HEAD
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-=======
->>>>>>> main
 #include "Bullet.h"
 #include "Components/StaticMeshComponent.h"
 #include <Runtime/Engine/Classes/Kismet/KismetMathLibrary.h>
 
-<<<<<<< HEAD
-// Sets default values
-ABullet::ABullet()
-{
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
-
-	ConstructorHelpers::FObjectFinder<UStaticMesh>MeshAsset(TEXT("StaticMesh'/Game/FPS_Weapon_Bundle/Weapons/Meshes/Ammunition/SM_Shell_45ap.SM_Shell_45ap'"));
-	UStaticMesh* Asset = MeshAsset.Object;
-
-	SetActorEnableCollision(false);
-
-	Mesh->SetStaticMesh(Asset);
-	RootComponent = Cast<USceneComponent>(Mesh);
-
-	FVector Scale = FVector(2, 2, 2);
-	Mesh->SetWorldScale3D(Scale);
-
-	FRotator Rotation = FRotator(0.f, -90.f, 0.f);
-
-	Mesh->AddWorldRotation(Rotation);
-=======
 ABullet::ABullet()
 {
 	PrimaryActorTick.bCanEverTick = true;
@@ -58,21 +28,12 @@ void ABullet::BeginPlay()
 
 	PrimaryActorTick.SetTickFunctionEnable(false);
 	bIsActive = false;
->>>>>>> main
 }
 
 void ABullet::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-<<<<<<< HEAD
-	if (RaycastHit.IsValidBlockingHit())
-	{
-		SetActorLocation(UKismetMathLibrary::VInterpTo_Constant(GetActorLocation(), RaycastHit.ImpactPoint, DeltaTime, 1400.f));
-	}
-}
-
-=======
 	const float MaxRangeSq = MaxRange * MaxRange;
 	const FVector Delta = TraceDirection * Speed * DeltaTime;
 	const FVector TraceEnd = CurrentTraceLocation + Delta;
@@ -81,7 +42,7 @@ void ABullet::Tick(float DeltaTime)
 
 	FHitResult Hit;
 	GetWorld()->LineTraceSingleByChannel(Hit, CurrentTraceLocation, TraceEnd, ECC_Pawn, QueryParams);
-	
+
 	if (Hit.bBlockingHit)
 	{
 		if (Hit.Actor != nullptr)
@@ -138,4 +99,3 @@ void ABullet::SetIsActive(const bool Value)
 	bIsActive = Value;
 	Mesh->SetVisibility(Value);
 }
->>>>>>> main
