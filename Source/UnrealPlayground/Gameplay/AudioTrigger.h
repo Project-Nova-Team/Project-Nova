025c013c24246f58cf019038b9ec3cdd444c7317
Brendan Lienau <BLienau@colum.edu>
2021-11-12 01:24:05 -0600

    Triggers

--- Source/UnrealPlayground/Gameplay/AudioTrigger.h
+++ Source/UnrealPlayground/Gameplay/AudioTrigger.h
@@ -1,53 +1,24 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/Actor.h"
-#include "Removable.h"
-#include <Runtime/Engine/Classes/Components/BoxComponent.h>
+#include "Trigger.h"
 #include "AudioTrigger.generated.h"
 
 class UAudioComponent;
-class USoundCue;
-class UGameplayStatistics;
 
 UCLASS()
-class UNREALPLAYGROUND_API AAudioTrigger : public AActor, public IRemovable
+class UNREALPLAYGROUND_API AAudioTrigger : public ATrigger
 {
 	GENERATED_BODY()
 	
 public:	
-	// Sets default values for this actor's properties
 	AAudioTrigger();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-private:
-
-	UBoxComponent* BoxTrigger;
-
-	virtual void RemoveSelf(AActor* Actor) override;
-
-	virtual void RestoreSelf(AActor* Actor) override;
-
-	/** Used to play audio. Set in bp*/
+	/** Component that plays the bark*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Volume")
 	UAudioComponent* AudioComponent;
 
-	/** Set true if this object should be disabled after player leaves the trigger.*/
-	UPROPERTY(EditAnywhere)
-	uint8 bDisappears : 1;
-
-	/** The actor that will activate this trigger.Set this to shooter. */
-	UPROPERTY(EditAnywhere)
-	AActor* TargetActor;
-
-	UFUNCTION(BlueprintCallable)
-	void SetComponents(UAudioComponent* AudioComponent);
-
-	// Overlap
-	UFUNCTION()
-	void BeginOverlap(AActor* OverlappedActor, AActor* OtherActor);
+	virtual void ExecuteTrigger(APawn* Sender) override;
 };
