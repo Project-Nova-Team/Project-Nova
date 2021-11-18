f19babd61615cbe4b303f81c518d48c3d8951f61
Brendan Lienau <BLienau@colum.edu>
2021-11-18 12:26:04 -0600

    spawn temp

--- Source/UnrealPlayground/AI/AIBase.cpp
+++ Source/UnrealPlayground/AI/AIBase.cpp
@@ -15,7 +15,13 @@ AAIBase::AAIBase()
 	Health = CreateDefaultSubobject<UHealthComponent>(TEXT("Health"));
 
 	AttackMoveSpeed = 375;
-	
+}
+
+void AAIBase::SetVisible(const bool Value)
+{
+	const ECollisionEnabled::Type Visibility = Value ? ECollisionEnabled::QueryAndPhysics : ECollisionEnabled::NoCollision;
+	GetCapsuleComponent()->SetCollisionEnabled(Visibility);
+	GetMesh()->SetVisibility(Value);
 }
 
 void AAIBase::BeginPlay()
@@ -37,8 +43,12 @@ void AAIBase::SetLifeStatus(const bool bIsAlive)
 	else
 	{
 		GetMesh()->SetSimulatePhysics(false);
-		GetMesh()->SetCollisionProfileName("HitScan");	
-		GetCapsuleComponent()->SetCollisionProfileName("Pawn");
+		GetMesh()->SetCollisionProfileName("NoCollision");	
+		GetCapsuleComponent()->SetCollisionProfileName("Pawn");		
 		GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+
+		//Hack. our meshes will be better
+		GetMesh()->SetRelativeLocation(FVector::ZeroVector);
+		GetMesh()->SetRelativeRotation(FRotator(0, -90, 0));
 	}
 }
