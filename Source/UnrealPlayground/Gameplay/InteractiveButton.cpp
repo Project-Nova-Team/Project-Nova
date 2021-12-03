7d0fddf628052d8ba89a74e50b22c65aa8185d68
David Nichol <5cents87@gmail.com>
2021-12-03 15:47:57 -0600

    Delete .Idea and Finished Buton

---
+++ Source/UnrealPlayground/Gameplay/InteractiveButton.cpp
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "InteractiveButton.h"
+#include "../ShooterGameMode.h"
+#include "Materials/MaterialInstanceDynamic.h"
+#include "Components/AudioComponent.h"
+#include "../Utility/DelayedActionManager.h"
+
+AInteractiveButton::AInteractiveButton()
+{
+	PushDepth = 10.f;
+	ButtonTransitionTime = .25f;
+
+	Panel = CreateDefaultSubobject<UStaticMeshComponent>("Panel");
+	SetRootComponent(Panel);
+
+	Button = CreateDefaultSubobject<UStaticMeshComponent>("Button");
+	Button->AttachToComponent(Panel, FAttachmentTransformRules::KeepRelativeTransform);
+
+	AudioComponent = CreateDefaultSubobject<UAudioComponent>("Audio");
+	AudioComponent->AttachToComponent(Panel, FAttachmentTransformRules::KeepRelativeTransform);
+
+	Arrow = CreateDefaultSubobject<UArrowComponent>("Arrow");
+	Arrow->AttachToComponent(Button, FAttachmentTransformRules::KeepRelativeTransform);
+
+	//Dynamic Material for Button is being set in BP
+}
+
+void AInteractiveButton::SetIsLocked(const bool Value)
+{
+	if (Value != bIsLocked)
+	{
+		bIsLocked = Value;
+	}
+}
+
+void AInteractiveButton::InteractionEvent(APawn* EventSender)
+{
+	MaybeChangeButtonState();
+}
+
+
+void AInteractiveButton::BeginPlay()
+{
+	Super::BeginPlay();
+	InitialOffset = Button->GetRelativeLocation();
+}
+
+void AInteractiveButton::MaybeChangeButtonState()
+{
+	// if not locked and current state is extended, retract
+	if (ShouldRetract())
+	{
+		State = EBS_Changing;
+		StartDelayedAction(EBS_Retracted);
+	}
+	// if not locked and current state is retracted, extend
+	else if (ShouldExtend())
+	{
+		State = EBS_Changing;
+		StartDelayedAction(EBS_Extended);
+	}
+}
+
+void AInteractiveButton::OverTimeTransition(const EButtonState TargetState)
+{
+	bIsMoving = true;
+
+	const FVector Direction = FVector::ForwardVector;
+
+	float Offset;
+
+	if (TargetState == EBS_Retracted)
+	{
+		Offset = FMath::Lerp(0.f, PushDepth, Handle->CurrentActionProgress);
+		Button->SetRelativeLocation(InitialOffset + Direction * Offset);
+	}
+	else
+	{
+		Offset = FMath::Lerp(PushDepth, 0.f, Handle->CurrentActionProgress);
+		Button->SetRelativeLocation(InitialOffset + Direction * Offset);
+	}
+
+	/*We've finished transitioning. Decide if we should start
+	closing or opening again because something might have occured during the transition*/
+	if (Handle->CurrentActionProgress >= 1.f)
+	{
+		bIsMoving = false;
+
+		State = TargetState;
+
+		if (State == EBS_Retracted)
+		{
+			AudioComponent->Play();
+			StartDelayedAction(EBS_Extended);
+		}
+	}
+}
+
+void AInteractiveButton::Retract()
+{
+
+}
+
+void AInteractiveButton::Extend()
+{
+}
+
+void AInteractiveButton::StartDelayedAction(EButtonState TargetState)
+{
+	Handle = GetWorld()->GetAuthGameMode<AShooterGameMode>()->GetDelayedActionManager()->StartOverTimeAction(
+		this, &AInteractiveButton::OverTimeTransition, ButtonTransitionTime, TargetState);
+}
---
+++ Source/UnrealPlayground/Gameplay/InteractiveButton.h
@@ -0,0 +1,110 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractiveObject.h"
+#include "Components/ArrowComponent.h"
+#include "InteractiveButton.generated.h"
+
+struct FDelayedActionHandle;
+
+class UAudioComponent;
+class UMaterialInstanceDynamic;
+
+UENUM()
+enum EButtonState
+{
+	EBS_Extended,
+	EBS_Retracted,
+	EBS_Changing
+};
+
+UCLASS()
+class UNREALPLAYGROUND_API AInteractiveButton : public AActor, public IInteractiveObject
+{
+	GENERATED_BODY()
+
+public:
+
+	AInteractiveButton();
+
+	void InteractionEvent(APawn* EventSender) override;
+
+	bool CanInteract() const override { return !bIsLocked && !bIsMoving; }
+
+	FInteractionPrompt& GetInteractionPrompt() override { return Prompt; }
+
+	/** Sets the lock status of this Button*/
+	UFUNCTION(BlueprintSetter)
+		void SetIsLocked(const bool Value);
+
+	/** Should this button be opening*/
+	FORCEINLINE bool ShouldRetract() const { return !bIsLocked && State == EBS_Extended; }
+
+	/** Should this Button be shutting.*/
+	FORCEINLINE bool ShouldExtend() const { return !bIsLocked && State == EBS_Retracted; }
+
+protected:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* Panel;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+		UStaticMeshComponent* Button;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh", meta = (AllowPrivateAccess = "true"))
+		UArrowComponent* Arrow;
+
+	UMaterialInstanceDynamic* EmissiveMaterial;
+
+	/** Component that plays the sound*/
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Sound")
+		UAudioComponent* AudioComponent;
+
+	void MaybeChangeButtonState();
+
+	void BeginPlay();
+
+	/** Button Retracts into the panel*/
+	void Retract();
+
+	/** Button Extends outward from the panel*/
+	void Extend();
+
+	void StartDelayedAction(EButtonState TargetState);
+
+	/** Initial offset of the button mesh to the panel*/
+	FVector InitialOffset;
+
+	/** How far this button retracts into the panel*/
+	UPROPERTY(EditAnywhere)
+		float PushDepth;
+
+	/** How long it takes for the button to retract*/
+	UPROPERTY(EditAnywhere)
+		float ButtonTransitionTime;
+
+	/** Whether or not the button is currently locked*/
+	UPROPERTY(EditAnywhere, BlueprintSetter = SetIsLocked, Category = "Door")
+		uint8 bIsLocked : 1;
+
+	/**This is mesh of button, not panel*/
+	UPROPERTY(BlueprintReadWrite)
+		UStaticMeshComponent* Mesh;
+
+	UPROPERTY(Editanywhere, BlueprintReadWrite, Category = "Interactable")
+		FInteractionPrompt Prompt;
+
+	/** Live state of this door*/
+	TEnumAsByte<EButtonState> State;
+
+	/** Delayed action handle for ease of LERP*/
+	FDelayedActionHandle* Handle;
+
+	/** Move Button*/
+	void OverTimeTransition(const EButtonState TargetState);
+
+	uint8 bIsMoving;
+};
