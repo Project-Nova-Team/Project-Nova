f94bd657f6c88ac2b987a182da9fe143c9052f28
Brendan Lienau <Kobakat3@gmail.com>
2022-03-18 11:47:42 -0500

    fuse box

--- Source/ProjectNova/Gameplay/InteractableFusebox.cpp
+++ Source/ProjectNova/Gameplay/InteractableFusebox.cpp
@@ -1,14 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Animation/AnimInstance.h"
 #include "InteractableFusebox.h"
+#include "Animation/AnimInstance.h"
 
-// Sets default values
+
 AInteractableFusebox::AInteractableFusebox()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
 	Mesh = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
 	SetRootComponent(Mesh);
 
